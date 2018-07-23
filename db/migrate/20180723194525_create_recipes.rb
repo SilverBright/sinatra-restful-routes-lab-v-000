328f296932c06170029882bee822dd3c3406d5fb
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-23 19:54:11 +0000

    automatically backed up by learn

--- db/migrate/20180723194525_create_recipes.rb
+++ db/migrate/20180723194525_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :dogs do |t|
       t.string :name
-      t.string :breed
+      t.string :ingredients
     end
   end
 end
