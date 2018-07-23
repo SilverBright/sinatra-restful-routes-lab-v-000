d5d4bb39c4d9039629c0aef341deaf594765b5f9
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-23 20:58:46 +0000

    automatically backed up by learn

--- app/views/show.rb
+++ app/views/show.rb
@@ -1,6 +1,6 @@
-<h3><%= @recipe.name %></h3>
-<p><%= @recipe.ingredients %></p>
-<p><%= @recipe.cook_time %></p>
+<h1><%= @recipe.name %></h1>
+<h2><%= @recipe.ingredients %></h2>
+<h3><%= @recipe.cook_time %></h3>
 
 <form action="/recipes/<%= @recipe.id %>/delete" method="post">
   <input id="hidden" type="hidden" name="_method" value="delete">
