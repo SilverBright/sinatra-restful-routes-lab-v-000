ac14eee83431a9a1f6480144f3a5bca710c27db7
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-23 20:43:28 +0000

    automatically backed up by learn

--- app/views/new.rb
+++ app/views/new.rb
@@ -1,6 +1,15 @@
-# <form action="/recipes" method="post">
-#   <input type="text" name="name">
-#   <input type="text" name="ingredients">
-#   <input type="text" name="cook_time">
-#   <input type="submit" value="submit">
-# </form>
+<form action="/recipes" method="post">
+  <div>
+    <label for="name">Recipe name:</label>
+    <input type="text" name="name" id="name">
+  </div>
+  <div>
+    <label for="ingredients">Recipe ingredients:</label>
+    <input type="text" name="ingredients" id="ingredients">
+  </div>
+  <div>
+    <label for="cook_time">Recipe cook time:</label>
+    <input type="text" name="cook_time" id="cook_time">
+  </div>
+  <input name="submit" type="submit" id="submit">
+</form>
